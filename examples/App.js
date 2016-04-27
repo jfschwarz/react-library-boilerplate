b9d6e7f48438458a9b76cdfe49ca95a94b479684
Jan-Felix <schwarz@mpango.de>
2016-04-27 14:35:56 +0200

    setup webpack-dev-server for examples page

---
+++ examples/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Foo } from 'react-library-boilerplate'  // eslint-disable-line import/no-unresolved
+
+export default function App() {
+  return (
+    <div>
+      <h1>Examples</h1>
+
+      <Foo name="world" />
+    </div>
+  )
+}
